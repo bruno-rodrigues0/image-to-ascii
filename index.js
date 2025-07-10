4801029acf454240b8477e725abbf7d02853ebf0
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-07-10 15:46:20 -0300

    modernizada no bagulho

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const imageToAscii = require('image-to-ascii');
+const asciify = require("asciify-image");
 
-let url = 'https://redeardente.com/wp-content/uploads/2023/07/quem-era-o-pai-do-tanjiro.webp';
+const options = {
+  fit:    'box',      // 'box' or 'width' or 'height'
+  width:  400,        // Width of the output (number)
+  height: 400,        // Height of the output (number)
+  color:  true,      // true to output color ASCII, false for black & white
+  c_ratio: 2,         // Character aspect ratio (default 2)
+  as_string: false,   // true to return as string, false for console output
+  px_background: true // true to use background color for pixels
+};
 
-imageToAscii(url, (err, image) => {
-    console.log(err || image);
-})
-
+asciify('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr8br4i6uaIGSXcj56kdWnT3PMdVrxsSZT2A&s', options)
+  .then(console.log)
+  .catch(console.error);
